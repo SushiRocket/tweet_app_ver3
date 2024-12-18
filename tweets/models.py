2804de05263fb05b7474b5e7c9b949558f758634
SushiRocket <kentamori27@gmail.com>
2024-12-18 16:44:30 +0900

    Tweetモデルにlikesを追加 TweetModelTest作成

--- tweets/models.py
+++ tweets/models.py
@@ -6,6 +6,10 @@ class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_tweets', blank=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.content[:20]}"
+    
+    def total_likes(self):
+        return self.likes.count()
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from.models import Tweet
 
 # Create your tests here.
+class TweetModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.tweet = Tweet.objects.create(content='Test Tweet', user=self.user)
+
+    def test_like_tweet(self):
+        self.tweet.likes.add(self.user)
+        self.assertEqual(self.tweet.total_likes(), 1)
